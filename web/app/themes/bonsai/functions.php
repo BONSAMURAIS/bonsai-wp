4892422fa6fa175acda051eb87a78d8965547f88
mj-adtention <mj@adtention.dk>
2025-02-24 11:10:57 +0100

    add marker IO

--- web/app/themes/bonsai/functions.php
+++ web/app/themes/bonsai/functions.php
@@ -17,3 +17,14 @@ require_once "includes/shortcodes/adt-navigation-shortcode.php";
 require_once "includes/shortcodes/adt-searchform-shortcode.php";
 require_once "includes/shortcodes/adt-issues-shortcode.php";
 
+add_action('wp_head', function(){
+    ?>
+    <script>
+        window.markerConfig = {
+            project: '67bc4524d5023171bf6592ec',
+            source: 'snippet',
+        };
+        !function(e,r,a){if(!e.__Marker){e.__Marker={};var t=[],n={__cs:t};["show","hide","isVisible","capture","cancelCapture","unload","reload","isExtensionInstalled","setReporter","clearReporter","setCustomData","on","off"].forEach(function(e){n[e]=function(){var r=Array.prototype.slice.call(arguments);r.unshift(e),t.push(r)}}),e.Marker=n;var s=r.createElement("script");s.async=1,s.src="https://edge.marker.io/latest/shim.js";var i=r.getElementsByTagName("script")[0];i.parentNode.insertBefore(s,i)}}(window,document);
+    </script>
+    <?php
+});
