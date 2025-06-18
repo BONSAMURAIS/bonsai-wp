7e5adbded61eae98221f399760da4f6dc2d2002d
kevin-nhan <kevin.nhan@lca-net.com>
2025-06-18 13:28:58 +0200

    fix: missing trailing slash

--- web/app/themes/bonsai/includes/functions/adt-enqueue-functions.php
+++ web/app/themes/bonsai/includes/functions/adt-enqueue-functions.php
@@ -12,7 +12,7 @@ add_action('wp_enqueue_scripts', function (): void {
     // wp_enqueue_style('flatsome-style', sprintf('%s/style.css', $baseUrl), ['flatsome-main'], $version);
 
     // wp_enqueue_script('adt-mobile-menu', sprintf('%s/dist/js/adt-mobile-menu-script.js', $baseUrl), ['jquery'], $version);
-    wp_enqueue_script('adt-faq-script', $baseUrl.'dist/js/adt-faq.js', ['jquery']);
+    wp_enqueue_script('adt-faq-script', $baseUrl.'/dist/js/adt-faq.js', ['jquery']);
 
     if (is_single()) {
         wp_enqueue_style('adt-blog-style', content_url('/themes/bonsai/dist/css/adt-blog-posts.css'));
