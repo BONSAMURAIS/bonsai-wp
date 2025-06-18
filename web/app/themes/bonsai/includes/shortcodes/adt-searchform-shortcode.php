2acf3ea8594ad4c0aea4be19f53192b76a40ec8c
kevin-nhan <kevin.nhan@lca-net.com>
2025-06-18 10:04:22 +0200

    test label

--- web/app/themes/bonsai/includes/shortcodes/adt-searchform-shortcode.php
+++ web/app/themes/bonsai/includes/shortcodes/adt-searchform-shortcode.php
@@ -375,7 +375,7 @@ add_shortcode( 'adt_searchform', function($atts) {
                                             <path d="M1.66174 5.67766L2.66705 4.67236L8.49982 10.5051L14.3326 4.67236L15.3379 5.67767L8.49982 12.5157L1.66174 5.67766Z" fill="#031819"/>
                                         </svg>
                                     </label>
-                                    <p>equal</p>
+                                    <p>rest</p>
                                 </div>
                             </div>
                             <div class="calculation-result">
