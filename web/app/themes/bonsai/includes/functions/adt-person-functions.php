a2ed1b2a8b81bedc05580d1d73f27a5c49cbf22d
kevin-nhan <kevin.nhan@lca-net.com>
2025-06-18 17:02:54 +0200

    fix data recipe

--- web/app/themes/bonsai/includes/functions/adt-person-functions.php
+++ web/app/themes/bonsai/includes/functions/adt-person-functions.php
@@ -115,7 +115,7 @@ function adt_get_person_footprint()
         'value' => $totalValue,
         'version' => $version,
         'unit_emission' => $footprintsArray[0]['unit_emission'],
-        'recipe' => json_encode($recipes['data']),
+        'recipe' => $recipes,
     ];
 
     $productCode = "";
