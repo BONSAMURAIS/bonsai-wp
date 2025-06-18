02d7cd2158c069cbe465f4002e0f31358b14604e
kevin-nhan <kevin.nhan@lca-net.com>
2025-06-18 17:12:59 +0200

    fix log

--- web/app/themes/bonsai/includes/functions/adt-person-functions.php
+++ web/app/themes/bonsai/includes/functions/adt-person-functions.php
@@ -91,7 +91,7 @@ function adt_get_person_footprint()
     // $householdRecipeData = adt_get_person_footprint_recipe('F_HOUS', $chosenCountry, $version);
     $recipes = adt_get_person_footprint_recipe($footprint['act_code'], $chosenCountry, $version);
     error_log("recipes");
-    error_log($recipes);
+    error_log(print_r($recipes));
     // $chinRecipeData = adt_get_person_footprint_recipe('I_CHIN', $chosenCountry, $version);
 
     /**
