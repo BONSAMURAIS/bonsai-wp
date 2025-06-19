ce492122cea17dac850a75d7f07fa7149a29aebd
kevin-nhan <kevin.nhan@lca-net.com>
2025-06-19 13:36:15 +0200

    log

--- web/app/themes/bonsai/includes/functions/adt-person-functions.php
+++ web/app/themes/bonsai/includes/functions/adt-person-functions.php
@@ -209,8 +209,8 @@ function adt_get_person_footprint_recipe(array $fdemand_categories, string $coun
                 $api_url = "https://lca.aau.dk/api/recipes-country/?page=" . $page_counter . "&act_code=" .$cat.$SEPARATOR.$act_code. "&region_code=" . $country . "&version=" . $version;
                 $response = wp_remote_get($api_url);
                 $body = wp_remote_retrieve_body($response);
-                error_log("body");
-                error_log($body);
+                error_log("response");
+                error_log($response);
                 
                 // Parse the JSON response
                 $result = json_decode($body, true);
