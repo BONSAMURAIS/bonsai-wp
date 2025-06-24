1f209abef2e11d30f0c3a5e46d5331f474534a52
kevin-nhan <kevin.nhan@lca-net.com>
2025-06-24 17:41:47 +0200

    modifies html

--- web/app/themes/bonsai/includes/shortcodes/adt-searchform-shortcode.php
+++ web/app/themes/bonsai/includes/shortcodes/adt-searchform-shortcode.php
@@ -312,8 +312,9 @@ add_shortcode( 'adt_searchform', function($atts) {
                                     <path d="M1.66174 5.67766L2.66705 4.67236L8.49982 10.5051L14.3326 4.67236L15.3379 5.67767L8.49982 12.5157L1.66174 5.67766Z" fill="#031819"/>
                                 </svg>
                             </label>
-                            <p>equal</p>
                         </div>
+                        <br/>
+                        <p>produces</p>
                         <p class="product-result"></p>
                         <p class="product-result-unit"></p>
                         <div class="tooltip-wrapper">
@@ -350,8 +351,9 @@ add_shortcode( 'adt_searchform', function($atts) {
                                             <path d="M1.66174 5.67766L2.66705 4.67236L8.49982 10.5051L14.3326 4.67236L15.3379 5.67767L8.49982 12.5157L1.66174 5.67766Z" fill="#031819"/>
                                         </svg>
                                     </label>
-                                    <p>equal</p>
                                 </div>
+                                <br/>
+                                <p>produces</p>
                             </div>
                             <div class="calculation-result">
                                 <p class="product-result"></p>
