d32e1c1fe2e6b3cb5512c1b074a65200b083d1ef
kevin-nhan <kevin.nhan@lca-net.com>
2025-06-24 16:28:12 +0200

    log

--- web/app/themes/bonsai/assets/js/adt-searchform.js
+++ web/app/themes/bonsai/assets/js/adt-searchform.js
@@ -6,7 +6,8 @@ jQuery(document).ready(function($){
     let test = $('#main-tile').find('.product-title');
     // let productCode = test[0].data('code');
     console.log("test product=",test);
-    console.log("test product[0]=",test[0]);
+    console.log("test product[0]=",test.first());
+    console.log("test product[0].attr=",test.first().attr("data-code"));
     // console.log("test product[0].attr=",test[0].attr("data-code"));
     // console.log("productCode=",productCode);
 
