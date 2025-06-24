c6585063d051d97177f5cb0fe0580aa5aa9509cb
kevin-nhan <kevin.nhan@lca-net.com>
2025-06-24 15:36:49 +0200

    log

--- web/app/themes/bonsai/assets/js/adt-searchform.js
+++ web/app/themes/bonsai/assets/js/adt-searchform.js
@@ -7,7 +7,9 @@ jQuery(document).ready(function($){
         console.log( "label-select" );
         let select = $(this).first();
         if (Array.isArray(select) && select.length <= 1){
+            console.log("$(this).children(':nth-child(2)')=",$(this).children(':nth-child(2)'));
             $(this).children(':nth-child(2)').hide();
+            console.log("hide $(this).children(':nth-child(2)')=",$(this).children(':nth-child(2)'));
         }
     });
 
