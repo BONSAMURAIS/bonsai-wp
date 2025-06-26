cf64d3c54676a4418b985a9c32a17d00978c9735
kevin-nhan <kevin.nhan@lca-net.com>
2025-06-26 10:12:54 +0200

    hide gtp20

--- web/app/themes/bonsai/includes/shortcodes/adt-searchform-shortcode.php
+++ web/app/themes/bonsai/includes/shortcodes/adt-searchform-shortcode.php
@@ -229,7 +229,7 @@ add_shortcode( 'adt_searchform', function($atts) {
                             <label class="select" for="climate-metric">
                                 <select id="climate-metric">
                                     <option value="GWP100">GWP100</option>
-                                    <option value="GTP20">GTP20</option>
+                                    <!-- <option value="GTP20">GTP20</option> -->
                                 </select>
                                 <svg class="select-arrow" width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M1.66174 5.67766L2.66705 4.67236L8.49982 10.5051L14.3326 4.67236L15.3379 5.67767L8.49982 12.5157L1.66174 5.67766Z" fill="#031819"/>
