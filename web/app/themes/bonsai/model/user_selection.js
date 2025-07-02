b25dfc8b1537c66211694ba25fbd3c7fead46593
kevin-nhan <kevin.nhan@lca-net.com>
2025-07-02 17:51:32 +0200

    rollback dafault value

--- web/app/themes/bonsai/model/user_selection.js
+++ web/app/themes/bonsai/model/user_selection.js
@@ -5,7 +5,7 @@ class UserSelection{
         this.year = jQuery('#year').val() ?? '1999';
         this.income_gpe = jQuery('#income-group').val();
         this.household_compo = jQuery('#household-composition').val();
-        this.climate_metric = jQuery('#climate-metric').val() == null ? "GWP100" : jQuery('#climate-metric').val();
+        this.climate_metric = jQuery('#climate-metric').val() ?? "GWP100";
         this.footprint_type = jQuery('#footprint-type input[name="footprint_type').val();
     }
 }
