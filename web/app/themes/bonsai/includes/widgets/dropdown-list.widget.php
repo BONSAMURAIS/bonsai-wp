a9fe5b047ea285b5a1ea64415f4538336dfa61e6
kevin-nhan <kevin.nhan@lca-net.com>
2025-07-03 16:05:56 +0200

    fix: test - use of file path

--- web/app/themes/bonsai/includes/widgets/dropdown-list.widget.php
+++ web/app/themes/bonsai/includes/widgets/dropdown-list.widget.php
@@ -3,18 +3,31 @@
 
         $default = array(
             'id' => 'id',
-            'list_options' => '[{"id": "1", "label": "One"}]',
+            'filepath' => '',
         );
-
         $a = shortcode_atts($default, $attr);
 
+        $json_file = $atts['filepath'];
+
+        if (!file_exists($json_file)) {
+            return '<p>JSON file not found.</p>';
+        }
+
+        $json_content = file_get_contents($json_file);
+        $list_options = json_decode($json_content, true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            return '<p>Invalid JSON data.</p>';
+        }
+
         $options = '';
-        foreach (json_decode($a['list_options']) as $option) {
+        foreach ($list_options as $option) {
             $id = isset($option['id']) ? esc_html($option['id']) : '';
             $label = isset($option['label']) ? esc_html($option['label']) : '';
             $options .= '<option value="' . $id . '">' . $label . '</option>';
         }
         $arrow_icon = do_shortcode('[arrow_icon]');
+
         return '<label class="select" for="'.$a['id'].'">
                     <select id="'.$a['id'].'">
                     '.$options.'
