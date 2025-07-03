700cc0e80defe51f450060dd8e2cee00144d72f4
kevin-nhan <kevin.nhan@lca-net.com>
2025-07-03 11:55:05 +0200

    tempo fix

--- web/app/themes/bonsai/includes/widgets/dropdown-list.widget.php
+++ web/app/themes/bonsai/includes/widgets/dropdown-list.widget.php
@@ -8,15 +8,11 @@
                     <a href="#info-household-composition">
                         Household composition
                     </a>
-                    <?= do_shortcode('') ?>
+
                 </div>
                 <label class="select" for="household-composition">
                     <select id="household-composition">
-                        <?php 
-                            foreach($household_compo_options as $option) {
-                                echo '<option value="'. $option['id'].  '">'. ucfirst($option['label']).'</option>';      
-                            }
-                        ?>
+
                     </select>
                     <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1.66174 5.67766L2.66705 4.67236L8.49982 10.5051L14.3326 4.67236L15.3379 5.67767L8.49982 12.5157L1.66174 5.67766Z" fill="#031819"/>
