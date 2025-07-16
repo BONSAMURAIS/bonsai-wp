ea5b92b77b456037895cdac615b3480d1f05bc49
kevin-nhan <kevin.nhan@lca-net.com>
2025-07-16 12:44:17 +0200

    fix: return promise

--- web/app/themes/bonsai/utils/api-call.js
+++ web/app/themes/bonsai/utils/api-call.js
@@ -31,42 +31,41 @@ export async function get_product(userSelection){
     });
 }
 
-export function get_person_footprint(userSelection){
+export async function get_person_footprint(userSelection){
     let act_code = userSelection.income_gpe+"_"+userSelection.household_compo; //fdemandCat will be prefixed in adt-person-functions.php
     console.log("act_code=",act_code);
     let autocomplete_input = jQuery('#autocomplete-input'); 
 
-    jQuery.ajax({
-        type: 'POST',
-        url: localize._ajax_url,
-        data: {
-            _ajax_nonce: localize._ajax_nonce,
-            action: 'adt_get_person_footprint', //reference in adt-person-functions.php
-            version: userSelection.db_version,
-            act_code: act_code,
-            metric: userSelection.climate_metric,
-            region_code: userSelection.countryCode,
-        },
-        beforeSend: Utils.displayLoading(),
-        success: function(response) {
+    return new Promise((resolve, reject) => { 
+        jQuery.ajax({
+            type: 'POST',
+            url: localize._ajax_url,
+            data: {
+                _ajax_nonce: localize._ajax_nonce,
+                action: 'adt_get_person_footprint', //reference in adt-person-functions.php
+                version: userSelection.db_version,
+                act_code: act_code,
+                metric: userSelection.climate_metric,
+                region_code: userSelection.countryCode,
+            },
+            beforeSend: Utils.displayLoading(),
+            success: function(response) {
+                console.log("response.data=",response.data)
+                jQuery('.loading').remove();
+                autocomplete_input.prop('disabled', false);
+                resolve(response.data);
+                
+            },
+            error: (response) => {
+                console.log("error: ",response);
+                let error_initMsg = jQuery('#initial-error-message');
 
-            console.log("response.data=",response.data)
-            resolve(response.data);
-            let error_msg = jQuery('.error-message');
-
-            jQuery('.loading').remove();
-            autocomplete_input.prop('disabled', false);
-            
-        },
-        error: (response) => {
-            console.log("error: ",response);
-            let error_initMsg = jQuery('#initial-error-message');
-
-            // Request was throttled
-            error_initMsg.html('<p>'+response.responseJSON?.data.error+'</p>');
-            error_initMsg.slideDown('fast');
-        }
-    });
+                // Request was throttled
+                error_initMsg.html('<p>'+response.responseJSON?.data.error+'</p>');
+                error_initMsg.slideDown('fast');
+            }
+        });
+    })
 
 }
 
