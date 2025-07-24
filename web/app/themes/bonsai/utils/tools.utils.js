5411760ec6e126b041a1e50c2bb82e957ca91e65
kevin-nhan <kevin.nhan@lca-net.com>
2025-07-24 19:13:06 +0200

    add animation

--- web/app/themes/bonsai/utils/tools.utils.js
+++ web/app/themes/bonsai/utils/tools.utils.js
@@ -30,6 +30,9 @@ export function show_search_results(id){
     jQuery(id).slideDown('slow', function(){
         // Might need something happening here
     });
+    jQuery('html, body').animate({
+        scrollTop: jQuery(id).offset().top - 90
+    }, CONST.ANIM.DURATION);
 }
 
 export function resizeTextToFit(text){
