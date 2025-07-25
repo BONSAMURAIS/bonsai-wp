0ef556f15743ffd846a25cd7319834032a8c01c4
kevin-nhan <kevin.nhan@lca-net.com>
2025-07-25 16:34:40 +0200

    test clean code

--- web/app/themes/bonsai/assets/js/adt-searchform.js
+++ web/app/themes/bonsai/assets/js/adt-searchform.js
@@ -433,7 +433,7 @@ jQuery(document).ready(function($){
         controlInput_value(amountInput);
         
         let numberInput = parseFloat(amountInput.val());
-        let maxNumber = parseInt(amountInput.attr('max'));
+        let maxNumber = 999999;
         let co2_result = amountInput.closest('div.choices')      // go up to the div wrapping 
                                     .find('p.co2-value');        // look inside for p.co2-value
         const co2_result_value = parseFloat(co2_result.data('normal_value'));
--- web/app/themes/bonsai/includes/shortcodes/adt-searchform-shortcode.php
+++ web/app/themes/bonsai/includes/shortcodes/adt-searchform-shortcode.php
@@ -217,7 +217,6 @@ add_shortcode( 'adt_searchform', function($atts) {
                                 </div>
                                 <div class="unit-select-wrapper">
                                     <input type="text" class="amount" id="amount"/>
-                                    <label class="select" for="amount"></label>
                                     <label class="select" for="unit">
                                         <svg class="unit-arrow" width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M1.66174 5.67766L2.66705 4.67236L8.49982 10.5051L14.3326 4.67236L15.3379 5.67767L8.49982 12.5157L1.66174 5.67766Z" fill="#031819"/>
