f1c49bac3121350682095bd451d8c4e38f22c264
kevin-nhan <kevin.nhan@lca-net.com>
2025-07-25 08:58:42 +0200

    add class unit

--- web/app/themes/bonsai/includes/shortcodes/adt-searchform-shortcode.php
+++ web/app/themes/bonsai/includes/shortcodes/adt-searchform-shortcode.php
@@ -225,7 +225,7 @@ add_shortcode( 'adt_searchform', function($atts) {
                                             <path d="M1.66174 5.67766L2.66705 4.67236L8.49982 10.5051L14.3326 4.67236L15.3379 5.67767L8.49982 12.5157L1.66174 5.67766Z" fill="#031819"/>
                                         </svg>
                                     </label>
-                                    <select id="unit"></select>
+                                    <select id="unit" class="unit"></select>
                                 </div>
                                 <br/>
                                 <p>produces</p>
