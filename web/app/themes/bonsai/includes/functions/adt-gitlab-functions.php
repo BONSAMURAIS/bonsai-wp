4b99c2facb3ded33f4e552f1e8a5141f326e4213
kevin-nhan <kevin.nhan@lca-net.com>
2025-07-30 10:14:33 +0200

    add gitlat api url

--- web/app/themes/bonsai/includes/functions/adt-gitlab-functions.php
+++ web/app/themes/bonsai/includes/functions/adt-gitlab-functions.php
@@ -4,13 +4,16 @@ defined('ABSPATH') || exit;
 
 use Roots\WPConfig\Config;
 
+$CONFIG = json_decode(file_get_contents(__DIR__.'/../../constants/config.json'), true);
+$GLOBALS['APIGITLAB_URL'] = $CONFIG['APIGITLAB_URL'];
+
 function adt_fetch_gitlab_issues() {
     $project_id = '<project-id>'; // Replace with your GitLab project ID
     $project_id = rawurlencode('bonsamurais/bonsai');
     // $project_id = rawurlencode('bonsamurais/bonsai/build/footprint_calculator');
     $project_id = rawurlencode('bonsamurais/bonsai/bug-report-website');
     $access_token = Config::get('GITLAB_TOKEN'); // Replace with your GitLab Personal Access Token
-    $api_url = "https://gitlab.com/api/v4/projects/{$project_id}/issues";
+    $api_url = $GLOBALS['APIGITLAB_URL']."/{$project_id}/issues";
     // GET https://gitlab.com/api/v4/projects/bonsamurais%2Fbonsai/issues
 
     // Add headers for authorization
