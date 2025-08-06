1ade10a8c59c3a8393e48481434e74d6e1d44e32
kevin-nhan <kevin.nhan@lca-net.com>
2025-08-06 11:07:36 +0200

    return json

--- web/app/themes/bonsai/includes/functions/adt-bonsai-api-functions.php
+++ web/app/themes/bonsai/includes/functions/adt-bonsai-api-functions.php
@@ -448,8 +448,7 @@ function get_product_name_by_code_api(){
     }
 
 
-    
-    return($result);
+    wp_send_json_success($result);
 }
 
 add_action('wp_ajax_get_product_name_by_code_api', 'get_product_name_by_code_api');
