f215b0e7aa182078fc3b5db75e842af3e499f8e4
kevin-nhan <kevin.nhan@lca-net.com>
2025-08-19 11:17:42 +0200

    test css

--- web/app/themes/bonsai/includes/shortcodes/adt-searchform-shortcode.php
+++ web/app/themes/bonsai/includes/shortcodes/adt-searchform-shortcode.php
@@ -200,10 +200,8 @@ add_shortcode( 'adt_searchform', function($atts) {
                         <div class="tile-corner">
                             <span class="adt-close"></span>
                             <div class="switch-field switch-field-container-contri-analysis">
-                                <input type="radio" id="basic-choice" name="contri-analysis" value="basic" checked/>
-                                <label for="basic-choice">Basic</label>
-                                <input type="radio" id="advanced-choice" name="contri-analysis" value="advanced" />
-                                <label for="advanced-choice">Advanced</label>
+                                <input type="radio" id="basic-choice" name="contri-analysis" value="basic" checked/><label for="basic-choice">Basic</label>
+                                <input type="radio" id="advanced-choice" name="contri-analysis" value="advanced" /><label for="advanced-choice">Advanced</label>
                             </div>
                         </div>
                         <div class="calculation-wrapper">
