a336b60e296c0aa0b0c53c44aff86dd1b788e270
kevin-nhan <kevin.nhan@lca-net.com>
2025-08-21 14:09:32 +0200

    log

--- web/app/themes/bonsai/includes/shortcodes/adt-issues-shortcode.php
+++ web/app/themes/bonsai/includes/shortcodes/adt-issues-shortcode.php
@@ -88,7 +88,7 @@ function adt_issues_shortcode() {
                         </td>
                         <td colspan="1" class="adt-issue-milestone"><?php echo esc_html($issue['milestone']['title'] ?? ""); ?></td>
                         <td colspan="1" class="adt-issue-status"><?php echo esc_html($issue['state']); ?></td>
-                        <td colspan="1" class="adt-issue-status"><?php echo esc_html($issue['created_at']); ?></td>
+                        <td colspan="1" class="adt-issue-date"><?php echo esc_html($issue['created_at']); ?></td>
                     </tr>
                 <?php endforeach; ?> 
                 <?php foreach ($closed_issues as $issue) : ?>
@@ -105,7 +105,7 @@ function adt_issues_shortcode() {
                         </td>
                         <td colspan="1" class="adt-issue-milestone"><?php echo esc_html($issue['milestone']['title'] ?? ""); ?></td>
                         <td colspan="1" class="adt-issue-status"><?php echo esc_html($issue['state']); ?></td>
-                        <td colspan="1" class="adt-issue-status"><?php echo esc_html($issue['created_at']); ?></td>
+                        <td colspan="1" class="adt-issue-date"><?php echo esc_html($issue['created_at']); ?></td>
                     </tr>
                 <?php endforeach; ?>
             </tbody>
@@ -121,6 +121,7 @@ echo "
             const table = document.getElementById('git-issue-table');
             const tbody = table.tBodies[0];
             const rows = Array.from(tbody.rows);
+            console.log('rows=',rows)
 
             const dateColIndex = 4;
 
