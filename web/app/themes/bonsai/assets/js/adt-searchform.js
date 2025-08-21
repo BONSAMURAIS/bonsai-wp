fe859f3d5629828944bb435f3973e73a00531317
kevin-nhan <kevin.nhan@lca-net.com>
2025-08-21 18:38:16 +0200

    fix: title

--- web/app/themes/bonsai/assets/js/adt-searchform.js
+++ web/app/themes/bonsai/assets/js/adt-searchform.js
@@ -91,6 +91,7 @@ jQuery(document).ready(function($){
                 let userSelection = new UserSelection;
                 userSelection.get_from_form();
                 let data = await API.get_person_footprint(userSelection);
+                data['title'] = "Emission per person";
                 await display_result("#product-analysis-content",data);
             } else {
                 $('#market').prop('checked', true).trigger('change'); // Fix applied here
