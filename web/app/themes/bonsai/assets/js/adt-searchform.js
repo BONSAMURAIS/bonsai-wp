76bf98ee22b4bb8ef04c366503a31c5efd74618a
kevin-nhan <kevin.nhan@lca-net.com>
2025-08-27 14:35:17 +0200

    fix: multiple error msg

--- web/app/themes/bonsai/assets/js/adt-searchform.js
+++ web/app/themes/bonsai/assets/js/adt-searchform.js
@@ -576,7 +576,9 @@ async function display_result(htmlclass, data){
     //error management
     let error_msg = jQuery('#error-message');
     if (data && (data.error && data.error.includes("Product not found") || data.title == "")) {
-        error_msg.append("<p id='error-message-content' class='error-message-content-decorator' >Selected footprint doesn't exist in the database. Try selecting a different product, location or footprint type.</p>");
+        if(jQuery('#error-message-content') == null){
+            error_msg.append("<p id='error-message-content' class='error-message-content-decorator' >Selected footprint doesn't exist in the database. Try selecting a different product, location or footprint type.</p>");
+        }
         error_msg.slideDown('fast');
         return false;
     }
