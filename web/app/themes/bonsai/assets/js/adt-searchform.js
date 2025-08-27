988c8a7bc74988122ef92ca4e6648f33b4a32d26
kevin-nhan <kevin.nhan@lca-net.com>
2025-08-27 14:38:15 +0200

    log

--- web/app/themes/bonsai/assets/js/adt-searchform.js
+++ web/app/themes/bonsai/assets/js/adt-searchform.js
@@ -576,6 +576,7 @@ async function display_result(htmlclass, data){
     //error management
     let error_msg = jQuery('#error-message');
     if (data && (data.error && data.error.includes("Product not found") || data.title == "")) {
+        console.log(jQuery('#error-message-content'))
         if(jQuery('#error-message-content') == null){
             error_msg.append("<p id='error-message-content' class='error-message-content-decorator' >Selected footprint doesn't exist in the database. Try selecting a different product, location or footprint type.</p>");
         }
