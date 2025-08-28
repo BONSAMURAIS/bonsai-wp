dec6a7c656c9f4b34bf497a4c286f2bd080b3dfc
kevin-nhan <kevin.nhan@lca-net.com>
2025-08-28 17:26:15 +0200

    fix add data attrivute

--- web/app/themes/bonsai/assets/js/adt-searchform.js
+++ web/app/themes/bonsai/assets/js/adt-searchform.js
@@ -598,7 +598,7 @@ async function display_result(htmlclass, data){
     let main_component = jQuery(htmlclass);
     //set title
     main_component.find('.product-title').first().text(Utils.capitalize(data["title"]));
-    main_component.find('.product-title').first().attr("code",dataCode);
+    main_component.find('.product-title').first().attr("data-code",dataCode);
     //set tags
     //TODO hardcode replacement
     let dataCode = data['flow_code'];
