1065f842ff8565bf069c14a2f536e1d52061364e
kevin-nhan <kevin.nhan@lca-net.com>
2025-09-02 11:03:24 +0200

    remove test

--- web/app/themes/bonsai/includes/functions/adt-bonsai-api-functions.php
+++ web/app/themes/bonsai/includes/functions/adt-bonsai-api-functions.php
@@ -192,7 +192,7 @@ function adt_get_product_recipe($productCode, $country, $version,$metric): array
         if (!isset($recipe['value_emission'])) {
             $recipe['value_emission'] = $recipe['value'];
         }
-        $recipe['value_emission'] = 1;//convert_footprint_value($recipe['unit_reference'],$recipe['value_emission']);
+        $recipe['value_emission'] = convert_footprint_value($recipe['unit_reference'],$recipe['value_emission']);
     }
 
     //sort per value
