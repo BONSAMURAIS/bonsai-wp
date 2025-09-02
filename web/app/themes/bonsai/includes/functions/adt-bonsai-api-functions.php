d968ec97125178b08c745a5ae91ab0b00d5a4b04
kevin-nhan <kevin.nhan@lca-net.com>
2025-09-02 10:59:10 +0200

    fix: add unit in multiply recipe values

--- web/app/themes/bonsai/includes/functions/adt-bonsai-api-functions.php
+++ web/app/themes/bonsai/includes/functions/adt-bonsai-api-functions.php
@@ -192,7 +192,7 @@ function adt_get_product_recipe($productCode, $country, $version,$metric): array
         if (!isset($recipe['value_emission'])) {
             $recipe['value_emission'] = $recipe['value'];
         }
-        convert_footprint_value($recipe['value']);
+        convert_footprint_value($recipe['unit_reference'],$recipe['value']);
     }
 
     //sort per value
