4a87cdeddd5fc871754fd34288e118173bdf1081
kevin-nhan <kevin.nhan@lca-net.com>
2025-09-02 11:23:43 +0200

    change value reformat

--- web/app/themes/bonsai/utils/tools.utils.js
+++ web/app/themes/bonsai/utils/tools.utils.js
@@ -8,6 +8,14 @@ export function capitalize(str) {
 }
 
 export function reformatValue(value){
+  // Keep 3 significant digits
+  const precise = Number.parseFloat(value).toPrecision(CONFIG.SIGNIFICANT_NB);
+
+  // Convert to number to remove scientific notation
+  const rounded = Number(precise);
+
+  return rounded;
+
     return new Intl.NumberFormat(CONFIG.NUMBERFORMAT, {
         minimumFractionDigits: 0,
         maximumFractionDigits: CONFIG.SIGNIFICANT_NB
