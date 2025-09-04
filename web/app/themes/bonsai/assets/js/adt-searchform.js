8da6b71b58c21e35d22c56a2b23942be1d78dcc3
kevin-nhan <kevin.nhan@lca-net.com>
2025-09-04 16:06:03 +0200

    disable search on magnifier icon

--- web/app/themes/bonsai/assets/js/adt-searchform.js
+++ web/app/themes/bonsai/assets/js/adt-searchform.js
@@ -306,7 +306,7 @@ jQuery(document).ready(function($){
     adt_initialize_local_search_history();
 
     // Search 
-    $('#btn-search, #search-icon').click(async function(e){
+    $('#btn-search').click(async function(e){
         e.preventDefault();
         console.log("on click ",jQuery(this).attr('id'))
         let selectedValue = $('input[name="footprint_type"]:checked').val();
