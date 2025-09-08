b840ab20574a91075bcff08dffdbdd8f68c81e2f
kevin-nhan <kevin.nhan@lca-net.com>
2025-09-08 19:45:10 +0200

    update: for recipe - add var household, income

--- web/app/themes/bonsai/includes/functions/adt-person-functions.php
+++ web/app/themes/bonsai/includes/functions/adt-person-functions.php
@@ -59,7 +59,7 @@ function adt_get_person_footprint(){
 
     $fdemand_categories = array('F_GOVE', 'F_HOUS', 'F_NPSH');
     $value = get_total_value($countryCode,$household_type, $income_group,$version,$metric);
-    $recipes = adt_get_person_footprint_recipe($fdemand_categories, $countryCode, $act_code, $version,$metric);
+    $recipes = adt_get_person_footprint_recipe($countryCode,$household_type, $income_group,$version,$metric);
 
     
     //sort per value
@@ -132,84 +132,83 @@ function get_total_value(string $countryCode, string $household_type, string $in
 add_action('wp_ajax_adt_get_person_footprint', 'adt_get_person_footprint');
 add_action('wp_ajax_nopriv_adt_get_person_footprint', 'adt_get_person_footprint');
 
-function adt_get_person_footprint_recipe(array $fdemand_categories, string $countryCode, string $act_code, int|string $version, string $metric): array
+function adt_get_person_footprint_recipe(string $countryCode, string $household_type, string $income_group, string $version, string $metric) : array
 { //something off here
     global $SEPARATOR;
     $recipeResult = [];
     
-    foreach ($fdemand_categories as $cat){
-        $url = $GLOBALS['APIURL'].'/recipes-country/?act_code='.$cat.$SEPARATOR.$act_code.'&region_reference='.$countryCode.'&version='.$version.'&metric='.$metric;
-        $recipeResponse = wp_remote_get($url);
+    $url = $GLOBALS['APIURL'].'/recipes-country/?region_reference='.$countryCode.'&version='.$version.'&metric='.$metric."&household_type=".$household_type."&income_group=".$income_group;
+    $recipeResponse = wp_remote_get($url);
+    
+    // Check for errors
+    if (is_wp_error($recipeResponse)) {
+        return [
+            'error_oui' => $recipeResponse->get_error_message()
+        ];
+    }
+    
+    // Get the response body
+    $body = wp_remote_retrieve_body($recipeResponse);
+    $result = json_decode($body, true);
+
+    $productCount = $result['count'];
+
+    if (empty($result)) {
+        return ['No person recipe found or an error occurred.'];
+    }
+    
+    if (array_key_exists('detail', $result)) {
+        return ['Error: ' . $result['detail']];
+    }
+
+    if (!empty($result['results'])) {
+        foreach ($recipeResult as $recipe) {
+            foreach ($result['results'] as $new_recipe_key => $new_recipe_val) {
+                
+                if ($recipe["inflow"] == $new_recipe_val["inflow"]){
+                    $recipe["value_emission"] += $new_recipe_val["value_emission"];
+                    unset($result['results'][$new_recipe_key]);
+                }
+            }
+        }
+        $recipeResult = array_merge($recipeResult, $result['results']);
+    }
+    
+    $pages = ceil($productCount / 100);
+
+    // TODO: Throttled again for loading through the pages?
+    for ($i = 1; $i <= $pages; $i++) {
+        $api_url = $GLOBALS['APIURL'].'/recipes-country/?page=' . $i . '&region_reference='.$countryCode.'&version='.$version.'&metric='.$metric."&household_type=".$household_type."&income_group=".$income_group;
+
+        $response = wp_remote_get($api_url);
         
-        // Check for errors
-        if (is_wp_error($recipeResponse)) {
-            return [
-                'error_oui' => $recipeResponse->get_error_message()
-            ];
+        if (is_wp_error($response)) {
+            continue;
         }
         
-        // Get the response body
-        $body = wp_remote_retrieve_body($recipeResponse);
+        $body = wp_remote_retrieve_body($response);
         $result = json_decode($body, true);
-
-        $productCount = $result['count'];
-
-        if (empty($result)) {
-            return ['No person recipe found or an error occurred.'];
-        }
         
-        if (array_key_exists('detail', $result)) {
-            return ['Error: ' . $result['detail']];
-        }
-
         if (!empty($result['results'])) {
             foreach ($recipeResult as $recipe) {
                 foreach ($result['results'] as $new_recipe_key => $new_recipe_val) {
-                    
                     if ($recipe["inflow"] == $new_recipe_val["inflow"]){
                         $recipe["value_emission"] += $new_recipe_val["value_emission"];
                         unset($result['results'][$new_recipe_key]);
+                        // break;
                     }
+    
                 }
             }
             $recipeResult = array_merge($recipeResult, $result['results']);
-        }
-        
-        $pages = ceil($productCount / 100);
-
-        // TODO: Throttled again for loading through the pages?
-        for ($i = 1; $i <= $pages; $i++) {
-            $api_url = $GLOBALS['APIURL']."/recipes-country/?page=" . $i . "&act_code=" .$cat.$SEPARATOR.$act_code. "&region_reference=" . $countryCode . "&version=" . $version."&metric=".$metric;
-            $response = wp_remote_get($api_url);
-            
-            if (is_wp_error($response)) {
-                continue;
-            }
-            
-            $body = wp_remote_retrieve_body($response);
-            $result = json_decode($body, true);
-            
-            if (!empty($result['results'])) {
-                foreach ($recipeResult as $recipe) {
-                    foreach ($result['results'] as $new_recipe_key => $new_recipe_val) {
-                        if ($recipe["inflow"] == $new_recipe_val["inflow"]){
-                            $recipe["value_emission"] += $new_recipe_val["value_emission"];
-                            unset($result['results'][$new_recipe_key]);
-                            // break;
-                        }
-     
-                    }
-                }
-                $recipeResult = array_merge($recipeResult, $result['results']);
-            }            
-        }
-        
-        // Handle potential errors in the recipeResponse
-        if (empty($recipeResult)) {
-            return [
-                'error' => 'No recipes found or an error occurred.'
-            ];
-        }
+        }            
+    }
+    
+    // Handle potential errors in the recipeResponse
+    if (empty($recipeResult)) {
+        return [
+            'error' => 'No recipes found or an error occurred.'
+        ];
     }
 
         //sort per value
