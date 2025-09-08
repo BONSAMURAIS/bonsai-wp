6e3378fcd6293abca5868d64716e648d525fc310
kevin-nhan <kevin.nhan@lca-net.com>
2025-09-08 19:49:35 +0200

    fix: fix: remove double sorting

--- web/app/themes/bonsai/includes/functions/adt-person-functions.php
+++ web/app/themes/bonsai/includes/functions/adt-person-functions.php
@@ -60,14 +60,6 @@ function adt_get_person_footprint(){
     $fdemand_categories = array('F_GOVE', 'F_HOUS', 'F_NPSH');
     $value = get_total_value($countryCode,$household_type, $income_group,$version,$metric);
     $recipes = adt_get_person_footprint_recipe($countryCode,$household_type, $income_group,$version,$metric);
-  
-    //sort per value
-    usort($recipes, function ($a, $b) {
-        return $b['value_emission'] <=> $a['value_emission']; //b before a for descending order
-    });
-
-    // get only the first 20 elements
-    $recipes = array_slice($recipes, 0, 20);
 
     $data = [
         'id' => $footprintsArray[0]['id'],
@@ -210,6 +202,10 @@ function adt_get_person_footprint_recipe(string $countryCode, string $household_
         ];
     }
 
+    //sort per value
+    usort($recipeResult, function ($a, $b) {
+        return $b['value_emission'] <=> $a['value_emission']; //b before a for descending order
+    });
     $recipeResult = array_slice($recipeResult, 0, 20);
 
     return $recipeResult;
