6ac9de850e48ec18800a387e744f7932925703d3
kevin-nhan <kevin.nhan@lca-net.com>
2025-09-09 11:45:39 +0200

    add space in preposition

--- web/app/themes/bonsai/assets/js/adt-searchform.js
+++ web/app/themes/bonsai/assets/js/adt-searchform.js
@@ -656,14 +656,14 @@ async function display_result(htmlclass, data){
         })
     }
     let displayed_unit = unit_ref;
-    let preposition = "of ";
+    let preposition = " of ";
     if(displayed_unit == CONST.UNIT.TONNES){
         displayed_unit = CONST.UNIT.KG;
     }else if(displayed_unit == CONST.UNIT.TONNES_SERVICE){
         displayed_unit = CONST.UNIT.TONNES;
     } else if (isPersonTab){
         displayed_unit = CONST.UNIT.PERSON_YEAR;
-        preposition = "in ";
+        preposition = " in ";
     }
     displayed_unit += preposition;
     main_component.find('.product-unit').first().text(displayed_unit);
