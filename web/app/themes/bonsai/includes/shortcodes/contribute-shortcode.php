2c1e56abef11c8d1bf403f9f4ccb6c0023dac983
kevin-nhan <kevin.nhan@lca-net.com>
2025-09-29 11:06:30 +0200

    add contribute shortcode for contribute page

---
+++ web/app/themes/bonsai/includes/shortcodes/contribute-shortcode.php
@@ -0,0 +1,26 @@
+<?php
+// Register the shortcode
+function contribute_shortcode() {
+    wp_enqueue_style('adt-issues-style', content_url('/themes/bonsai/dist/css/adt-issues.css'));
+
+    // Start output buffering
+    ob_start();
+    ?>
+
+    
+    <div class="flex issues-search">
+        <input type="text" id="search" placeholder="Search issues" />
+        <input type="text" id="labels" placeholder="Labels" readonly />
+        <input type="text" id="milestones" placeholder="Milestones" readonly />
+        <a href="https://gitlab.com/bonsamurais/bonsai/bug-report-website/-/issues/new" class="button primary" target="_blank">                  
+            New data contribution
+        </a>
+    </div>
+
+<?php
+
+    }
+
+    // Add the shortcode
+    add_shortcode('contribute', 'contribute_shortcode');
+?>
