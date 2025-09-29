234d7cb870224bc1386907733dfdfebf26cdf49c
kevin-nhan <kevin.nhan@lca-net.com>
2025-09-29 11:15:27 +0200

    declare shortcode for wordpress

--- web/app/themes/bonsai/functions.php
+++ web/app/themes/bonsai/functions.php
@@ -20,6 +20,7 @@ require_once "includes/functions/adt-download-functions.php";
 require_once "includes/shortcodes/adt-navigation-shortcode.php";
 require_once "includes/shortcodes/adt-searchform-shortcode.php";
 require_once "includes/shortcodes/adt-issues-shortcode.php";
+require_once "includes/shortcodes/contribute-shortcode.php";
 
 // icons
 require_once "includes/icons/arrow.icon.php";
