c0e7b28157803c043f5b7ff3c2eeeb2eadc4babb
kevin-nhan <kevin.nhan@lca-net.com>
2025-10-02 15:04:24 +0200

    add download template code

--- web/app/themes/bonsai/includes/shortcodes/download-template-shortcode.php
+++ web/app/themes/bonsai/includes/shortcodes/download-template-shortcode.php
@@ -1,21 +1,40 @@
 <?php
-// Register the shortcode
-function download_template_shortcode() {
-    wp_enqueue_style('adt-issues-style', content_url('/themes/bonsai/dist/css/adt-issues.css'));
+    add_shortcode('secure_download', 'secure_static_download_shortcode');
+    add_action('admin_post_nopriv_download_file', 'serve_static_file');
 
-    // Start output buffering
-    ob_start();
-    ?>
+    function serve_static_file() {
+        $filename = 'Template_PPF_v1.xlsx';
+        $filepath = __DIR__.'/../../../uploads/'.$filename;
 
-    <a download>
-        Download template
-    </a>
+        if (!file_exists($filepath)) {
+            wp_die('File not found.');
+        }
 
-<?php
-    // Return the buffered content
-    return ob_get_clean();
+        $mime = mime_content_type($filepath);
+        $allowed_mimes = ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
+
+        if (!in_array($mime, $allowed_mimes)) {
+            wp_die('File type not allowed.');
+        }
+
+        header('Content-Description: File Transfer');
+        header('Content-Type: ' . $mime);
+        header('Content-Disposition: attachment; filename="' . $filename . '"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($filepath));
+        header('X-Content-Type-Options: nosniff');
+
+        readfile($filepath);
+        exit;
     }
 
-    // Add the shortcode
-    add_shortcode('download-data-contribute-template-shortcode-wp', 'download_template_shortcode');
+    // Shortcode: [download-data-contribute-template-shortcode-wp]
+    function secure_static_download_shortcode() {
+        $url = admin_url('admin-post.php?action=download_file');
+        return '<a href="' . esc_url($url) . '" download="'.$filename.'">Download</a>';
+    }
+
+    add_shortcode('download-data-contribute-template-shortcode-wp', 'secure_static_download_shortcode');
 ?>
