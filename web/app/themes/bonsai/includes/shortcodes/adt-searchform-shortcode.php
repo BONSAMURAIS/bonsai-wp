e2bfe237a6a79dcea3a5cfb70defd7bfec3aab17
kevin-nhan <kevin.nhan@lca-net.com>
2025-10-07 16:22:14 +0200

    invert arrow for location

--- web/app/themes/bonsai/includes/shortcodes/adt-searchform-shortcode.php
+++ web/app/themes/bonsai/includes/shortcodes/adt-searchform-shortcode.php
@@ -144,11 +144,11 @@ add_shortcode( 'adt_searchform', function($atts) {
                                     <div class="product-parameters">
                                         <label class="select" for="location">
                                             <select id="location">
+                                                <?= do_shortcode('[arrow_icon]')?>
                                                 <?php foreach($locationsArray as $location): ?>
                                                     <option value="<?php echo $location['code']; ?>"><?php echo $location['name']; ?></option>
                                                     <?php endforeach; ?>
                                                 </select>
-                                                <?= do_shortcode('[arrow_icon]')?>
                                             </label>
                                         <?= do_shortcode('[dropdown_list id="year" filepath="'.__DIR__.'/../../dropdown_options/year.json"]')?>
                                         <?= do_shortcode('[dropdown_list id="climatemetric" filepath="'.__DIR__.'/../../dropdown_options/climate_metric.json"]')?>
