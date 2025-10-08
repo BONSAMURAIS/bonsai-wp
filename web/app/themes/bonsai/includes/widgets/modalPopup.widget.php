a38872c770b6e3fa7fb96223d59835c0647d245c
kevin-nhan <kevin.nhan@lca-net.com>
2025-10-08 16:35:58 +0200

    restructure pop up

--- web/app/themes/bonsai/includes/widgets/modalPopup.widget.php
+++ web/app/themes/bonsai/includes/widgets/modalPopup.widget.php
@@ -6,11 +6,21 @@
             '<div id="modal-content">
                 <span id="closeModal" class="close">&times;</span>
                 <p>Hurray</p>'
+                .'<p>'
                 .do_shortcode('[block id="location-info-popup"]')
+                .'</p>'
+                .'<p>'
                 .do_shortcode('[block id="year-info-popup"]')
+                .'</p>'
+                .'<p>'
                 .do_shortcode('[block id="climate-metric-info-popup"]')
+                .'</p>'
+                .'<p>'
                 .do_shortcode('[block id="database-version-info-popup"]')
-                .do_shortcode('[block id="footprint-info-popup"]').
+                .'</p>'
+                .'<p>'
+                .do_shortcode('[block id="footprint-info-popup"]')
+                .'</p>'.
             '</div>';
     }
 
