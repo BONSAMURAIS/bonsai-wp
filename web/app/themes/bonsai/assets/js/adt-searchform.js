e37eef147e33b70584c9b17fcad15b316a01b4f9
kevin-nhan <kevin.nhan@lca-net.com>
2025-10-13 17:21:48 +0200

    log

--- web/app/themes/bonsai/assets/js/adt-searchform.js
+++ web/app/themes/bonsai/assets/js/adt-searchform.js
@@ -398,9 +398,11 @@ async function display_result(htmlclass, data){
     main_component.find('.product-title').first().attr("data-code",data['flow_code'] ?? "person");
     main_component.find('.product-title').first().attr("data-uuid",data['uuid']);
     //set location list of dropdown
+    console.log("main_component.find('.location').first()[0]=",main_component.find('.location').first()[0])
     console.log("data['list_locations']=",data['list_locations'])
     for (const location of data['list_locations']){
         console.log("location=",location)
+        console.log("location['name']=",location['name'])
         main_component.find('.location').first()[0].append(jQuery('<option>', {
             text: location['name'],
             value: location['code']
