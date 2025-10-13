0927931d365455cb8ffb6dd79fc7976c48919774
kevin-nhan <kevin.nhan@lca-net.com>
2025-10-13 18:54:45 +0200

    fix: remove unnecessary and wrong code

--- web/app/themes/bonsai/assets/js/adt-searchform.js
+++ web/app/themes/bonsai/assets/js/adt-searchform.js
@@ -640,10 +640,6 @@ function adt_dynamic_search_input(list_product, list_product_title)
             } else if (e.key === 'Enter' && jQuery('#autocomplete-input').val() !== '') {
                 e.preventDefault();
                 let data = await API.get_product_footprint_by_search(jQuery('#autocomplete-input').val());
-
-                if(selectedValue === 'person'){
-                    data['title'] = "Emission per person";
-                }
                 // adt_push_parameter_to_url(userSelection);
                 await display_result("#product-analysis-content",data);
             }
