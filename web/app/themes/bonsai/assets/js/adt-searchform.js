a6325cc8fc85072f6cdaaa2446b3f1189820fa85
kevin-nhan <kevin.nhan@lca-net.com>
2025-10-13 18:53:47 +0200

    fix: declare var

--- web/app/themes/bonsai/assets/js/adt-searchform.js
+++ web/app/themes/bonsai/assets/js/adt-searchform.js
@@ -639,7 +639,7 @@ function adt_dynamic_search_input(list_product, list_product_title)
                 markCurrentItem($items);
             } else if (e.key === 'Enter' && jQuery('#autocomplete-input').val() !== '') {
                 e.preventDefault();
-                data = await API.get_product_footprint_by_search(jQuery('#autocomplete-input').val());
+                let data = await API.get_product_footprint_by_search(jQuery('#autocomplete-input').val());
 
                 if(selectedValue === 'person'){
                     data['title'] = "Emission per person";
