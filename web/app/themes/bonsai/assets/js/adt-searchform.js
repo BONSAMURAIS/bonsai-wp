1ce68b807ca4ed6d914155f023346f1a38a15d10
kevin-nhan <kevin.nhan@lca-net.com>
2025-10-27 16:24:40 +0100

    test

--- web/app/themes/bonsai/assets/js/adt-searchform.js
+++ web/app/themes/bonsai/assets/js/adt-searchform.js
@@ -456,7 +456,7 @@ async function display_result(htmlclass, data){
 
     if(data["unit_reference"]=="TJ"){
 
-        Utils.selectOptionByText(unit_options.first()[0],"GJ");
+        Utils.selectOptionByText(unit_options.first()[0],"kwh");
     }
 
     let displayed_unit = data["unit_reference"];
