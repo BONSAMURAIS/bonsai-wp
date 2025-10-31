7c67b096d5ecb867d7eb91fc498b41e3a0a13347
kevin-nhan <kevin.nhan@lca-net.com>
2025-10-31 15:06:59 +0100

    roll back

--- web/app/themes/bonsai/utils/tools.utils.js
+++ web/app/themes/bonsai/utils/tools.utils.js
@@ -159,8 +159,7 @@ export function getUnitContriAnalysis(selectedUnit, unit_ref){
                 break;
             case CONST.UNIT.TONNES.toLowerCase():
             case CONST.UNIT.TONNES_SERVICE.toLowerCase():
-                const unit_tonnes = String(CONST.UNIT.TONNES).replace("tonnes", "tonne");
-                finalUnit = {ratio:1,label:unit_tonnes};
+                finalUnit = {ratio:1,label:CONST.UNIT.TONNES};
                 break;
         }
     }
