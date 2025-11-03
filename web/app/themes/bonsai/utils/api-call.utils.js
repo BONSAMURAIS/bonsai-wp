fa0a482e75bf2465d5489caa85b34ac1d514e930
kevin-nhan <kevin.nhan@lca-net.com>
2025-11-03 10:51:01 +0100

    fix: set contri-analysis in local storage

--- web/app/themes/bonsai/utils/api-call.utils.js
+++ web/app/themes/bonsai/utils/api-call.utils.js
@@ -17,7 +17,7 @@ export async function get_product_footprint_by_search(query){
                 Utils.hideLoading();
                 console.log("by search");
                 console.log(response);
-                //localStorage.setItem('emission_contriAnalysis', JSON.stringify(response.data.recipe));
+                localStorage.setItem('emission_contriAnalysis', JSON.stringify(response.data.recipe));
                 resolve(response.data);
             },
             error: (error) => {
