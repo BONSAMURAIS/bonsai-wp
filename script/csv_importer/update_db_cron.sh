f1ab7ef0a8b13c985578a435783bf370b8f5bdd4
kevin-nhan <kevin.nhan@lca-net.com>
2025-11-18 17:07:31 +0100

    add sh script

---
+++ script/csv_importer/update_db_cron.sh
@@ -0,0 +1,11 @@
+echo "START"
+echo "Virtual environment: activate"
+source venv/bin/activate
+echo "Virtual environment: activated"
+echo "Start uploading data"
+python3 main.py
+echo "Finish uploading data"
+echo "Virtual environment: deactivate"
+deactivate
+echo "Virtual environment: deactivated"
+echo "END"
