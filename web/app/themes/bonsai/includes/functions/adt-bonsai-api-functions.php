5bb3f3e32df3e984df2acc170d92853a0dd42c43
kevin-nhan <kevin.nhan@lca-net.com>
2025-11-27 16:16:15 +0100

    fix: var

--- web/app/themes/bonsai/includes/functions/adt-bonsai-api-functions.php
+++ web/app/themes/bonsai/includes/functions/adt-bonsai-api-functions.php
@@ -436,7 +436,7 @@ function call_product_footprint_api(string $productCode, string $countryCode, st
         'country' => $country,
         "unit_reference" => $unit_reference,
         "unit_emission" => $unit_emission,
-        'version' => $newestVersion,
+        'version' => $version,
         'all_data' => $footprint,
         'id' => $footprint['id'],
         'list_locations' => adt_get_locations(),//$result["locations"],
