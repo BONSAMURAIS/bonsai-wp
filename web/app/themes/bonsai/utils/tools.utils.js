875dc12f04060ad6547cb75d430b4eabfe5d2c75
kevin-nhan <kevin.nhan@lca-net.com>
2025-12-02 17:04:48 +0100

    change ratio tj to gj

--- web/app/themes/bonsai/utils/tools.utils.js
+++ web/app/themes/bonsai/utils/tools.utils.js
@@ -169,7 +169,7 @@ export function getUnitContriAnalysis(selectedUnit, unit_inflow, unit_reference)
     }else{//emission in tonnes
         switch (unit_inflow){ 
             case CONST.UNIT.TJ.toLowerCase():
-                finalUnit = {ratio:1e-3,label:CONST.UNIT.GJ};
+                finalUnit = {ratio:1e-6,label:CONST.UNIT.GJ};
                 break;
             case CONST.UNIT.ITEMS.toLowerCase():
                 finalUnit = {ratio:1,label:CONST.UNIT.ITEMS};
